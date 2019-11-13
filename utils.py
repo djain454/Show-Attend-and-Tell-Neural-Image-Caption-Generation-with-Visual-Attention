291ee8e66f677886a9bdb6f81874caa97c1fdbe0
djain454 <f20170935@pilani.bits-pilani.ac.in>
2019-11-13 18:27:24 +0530

    utils.py added

---
+++ utils.py
@@ -0,0 +1,37 @@
+import torch
+
+class AverageMeter(object):
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(preds, targets, k):
+    batch_size = targets.size(0)
+    _, pred = preds.topk(k, 1, True, True)
+    correct = pred.eq(targets.view(-1, 1).expand_as(pred))
+    correct_total = correct.view(-1).float().sum()
+    return correct_total.item() * (100.0 / batch_size)
+
+
+def calculate_caption_lengths(word_dict, captions):
+    lengths = 0
+    for caption_tokens in captions:
+        for token in caption_tokens:
+            if token in (word_dict['<start>'], word_dict['<eos>'], word_dict['<pad>']):
+                continue
+            else:
+                lengths += 1
+    return lengths
