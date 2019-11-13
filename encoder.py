234d633f7abc9ce365523f159c4c965586e3906e
djain454 <f20170935@pilani.bits-pilani.ac.in>
2019-11-13 18:25:38 +0530

    encoder.py added

---
+++ encoder.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+from torchvision.models import densenet161, resnet152, vgg19,mobilenet_v2
+
+
+class Encoder(nn.Module):
+    def __init__(self, network='vgg19'):
+        super(Encoder, self).__init__()
+        self.network = network
+        if network == 'resnet152':
+            self.net = resnet152(pretrained=True)
+            self.net = nn.Sequential(*list(self.net.children())[:-2])
+            self.dim = 2048
+        elif network == 'densenet161':
+            self.net = densenet161(pretrained=True)
+            self.net = nn.Sequential(*list(list(self.net.children())[0])[:-1])
+            self.dim = 1920
+        elif network == 'mobilenet_v2':
+            self.net = densenet161(pretrained=True)
+            self.net = nn.Sequential(*list(self.net.features.children())[:-1])
+            self.dim = 224
+        else:
+            self.net = vgg19(pretrained=True)
+            self.net = nn.Sequential(*list(self.net.features.children())[:-1])
+            self.dim = 512
+
+    def forward(self, x):
+        x = self.net(x)
+        x = x.permute(0, 2, 3, 1)
+        x = x.view(x.size(0), -1, x.size(-1))
+        return x
